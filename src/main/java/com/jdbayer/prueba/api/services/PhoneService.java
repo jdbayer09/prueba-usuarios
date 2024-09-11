9ebc023c83607695ebbf0864ede08cb78d950c92
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 18:33:47 -0500

    Funcion para crear o actualizar una lista de telefonos

--- src/main/java/com/jdbayer/prueba/api/services/PhoneService.java
+++ src/main/java/com/jdbayer/prueba/api/services/PhoneService.java
@@ -1,8 +1,12 @@
 package com.jdbayer.prueba.api.services;
 
 import com.jdbayer.prueba.api.models.dto.PhoneDTO;
+import com.jdbayer.prueba.api.models.dto.UserDTO;
+
+import java.util.List;
 
 public interface PhoneService {
-    PhoneDTO create(PhoneDTO phone);
-    PhoneDTO update(PhoneDTO phone);
+    PhoneDTO create(PhoneDTO phone, UserDTO user);
+    PhoneDTO update(PhoneDTO phone, UserDTO user);
+    List<PhoneDTO> saveAll(List<PhoneDTO> phones, UserDTO user);
 }
--- src/main/java/com/jdbayer/prueba/api/services/impl/PhoneServiceImpl.java
+++ src/main/java/com/jdbayer/prueba/api/services/impl/PhoneServiceImpl.java
@@ -2,13 +2,18 @@ package com.jdbayer.prueba.api.services.impl;
 
 import com.jdbayer.prueba.api.exceptions.NotExistPhoneException;
 import com.jdbayer.prueba.api.models.dto.PhoneDTO;
+import com.jdbayer.prueba.api.models.dto.UserDTO;
 import com.jdbayer.prueba.api.models.entities.PhoneEntity;
 import com.jdbayer.prueba.api.models.mappers.PhoneMapper;
+import com.jdbayer.prueba.api.models.mappers.UserMapper;
 import com.jdbayer.prueba.api.repositories.PhoneRepository;
 import com.jdbayer.prueba.api.services.PhoneService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -16,28 +21,52 @@ public class PhoneServiceImpl implements PhoneService {
 
     private final PhoneRepository phoneRepository;
     private final PhoneMapper phoneMapper;
+    private final UserMapper userMapper;
+
+    private static final String ERROR_NOT_FOUND_PHONE = "Phone not found";
 
     @Override
     @Transactional
-    public PhoneDTO create(PhoneDTO phone) {
-        var phoneEntity = configurePhoneEntity(phone, new PhoneEntity());
-        return phoneMapper.entityToDto(phoneEntity);
+    public PhoneDTO create(PhoneDTO phone, UserDTO user) {
+        var phoneEntity = configurePhoneEntity(phone, new PhoneEntity(), user);
+        return phoneMapper.entityToDto(phoneRepository.save(phoneEntity));
     }
 
     @Override
     @Transactional
-    public PhoneDTO update(PhoneDTO phone) {
+    public PhoneDTO update(PhoneDTO phone, UserDTO user) {
         var phoneEntity = configurePhoneEntity(
                     phone,
                     phoneRepository.findById(
-                            phone.getId()).orElseThrow(() -> new NotExistPhoneException("Este telefono no existe")));
-        return phoneMapper.entityToDto(phoneEntity);
+                            phone.getId()).orElseThrow(() -> new NotExistPhoneException(ERROR_NOT_FOUND_PHONE)), user);
+        return phoneMapper.entityToDto(phoneRepository.save(phoneEntity));
     }
 
-    private PhoneEntity configurePhoneEntity(PhoneDTO phoneDTO, PhoneEntity phoneEntity) {
+    @Override
+    @Transactional
+    public List<PhoneDTO> saveAll(List<PhoneDTO> phones, UserDTO user) {
+        var listPhones = phones.stream()
+                .map(phone -> {
+                    var id = phone.getId();
+                    if (id == null) {
+                        return configurePhoneEntity(phone, new PhoneEntity(), user);
+                    } else {
+                        return configurePhoneEntity(phone, phoneRepository.findById(
+                                phone.getId()).orElseThrow(() -> new NotExistPhoneException(ERROR_NOT_FOUND_PHONE)), user);
+                    }
+                })
+                .toList();
+        return StreamSupport
+                .stream(phoneRepository.saveAll(listPhones).spliterator(), false)
+                .map(phoneMapper::entityToDto)
+                .toList();
+    }
+
+    private PhoneEntity configurePhoneEntity(PhoneDTO phoneDTO, PhoneEntity phoneEntity, UserDTO user) {
         phoneEntity.setNumber(phoneDTO.getNumber());
         phoneEntity.setCityCode(phoneDTO.getCityCode());
         phoneEntity.setCountryCode(phoneDTO.getCountryCode());
-        return phoneRepository.save(phoneEntity);
+        phoneEntity.setUser(userMapper.dtoToEntity(user));
+        return phoneEntity;
     }
 }
