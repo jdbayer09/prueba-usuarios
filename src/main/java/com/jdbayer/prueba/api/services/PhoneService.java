644f2c2203b920a491e00bbbf93b067df4afd8a7
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 18:14:11 -0500

    Configuracion del servicio de telefonos donde podemos crear y actualizar dicho telefono.

--- src/main/java/com/jdbayer/prueba/api/services/PhoneService.java
+++ src/main/java/com/jdbayer/prueba/api/services/PhoneService.java
@@ -1,4 +1,8 @@
 package com.jdbayer.prueba.api.services;
 
+import com.jdbayer.prueba.api.models.dto.PhoneDTO;
+
 public interface PhoneService {
+    PhoneDTO create(PhoneDTO phone);
+    PhoneDTO update(PhoneDTO phone);
 }
--- src/main/java/com/jdbayer/prueba/api/services/impl/PhoneServiceImpl.java
+++ src/main/java/com/jdbayer/prueba/api/services/impl/PhoneServiceImpl.java
@@ -1,4 +1,43 @@
 package com.jdbayer.prueba.api.services.impl;
 
-public class PhoneServiceImpl {
+import com.jdbayer.prueba.api.exceptions.NotExistPhoneException;
+import com.jdbayer.prueba.api.models.dto.PhoneDTO;
+import com.jdbayer.prueba.api.models.entities.PhoneEntity;
+import com.jdbayer.prueba.api.models.mappers.PhoneMapper;
+import com.jdbayer.prueba.api.repositories.PhoneRepository;
+import com.jdbayer.prueba.api.services.PhoneService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PhoneServiceImpl implements PhoneService {
+
+    private final PhoneRepository phoneRepository;
+    private final PhoneMapper phoneMapper;
+
+    @Override
+    @Transactional
+    public PhoneDTO create(PhoneDTO phone) {
+        var phoneEntity = configurePhoneEntity(phone, new PhoneEntity());
+        return phoneMapper.entityToDto(phoneEntity);
+    }
+
+    @Override
+    @Transactional
+    public PhoneDTO update(PhoneDTO phone) {
+        var phoneEntity = configurePhoneEntity(
+                    phone,
+                    phoneRepository.findById(
+                            phone.getId()).orElseThrow(() -> new NotExistPhoneException("Este telefono no existe")));
+        return phoneMapper.entityToDto(phoneEntity);
+    }
+
+    private PhoneEntity configurePhoneEntity(PhoneDTO phoneDTO, PhoneEntity phoneEntity) {
+        phoneEntity.setNumber(phoneDTO.getNumber());
+        phoneEntity.setCityCode(phoneDTO.getCityCode());
+        phoneEntity.setCountryCode(phoneDTO.getCountryCode());
+        return phoneRepository.save(phoneEntity);
+    }
 }
