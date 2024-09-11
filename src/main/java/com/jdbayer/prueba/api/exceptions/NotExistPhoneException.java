3821b305323366c748b5080467b95050353d8abb
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 18:32:38 -0500

    correccion de serializable de exception

--- src/main/java/com/jdbayer/prueba/api/exceptions/NotExistPhoneException.java
+++ src/main/java/com/jdbayer/prueba/api/exceptions/NotExistPhoneException.java
@@ -1,6 +1,11 @@
 package com.jdbayer.prueba.api.exceptions;
 
+import java.io.Serial;
+
 public class NotExistPhoneException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 4671445210509805067L;
+
     public NotExistPhoneException(String message) {
         super(message);
     }
