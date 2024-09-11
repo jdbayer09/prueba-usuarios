36c694e1c263b4415a67b70c5715259d4a8777ab
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 17:08:42 -0500

    initial Commit

---
+++ src/main/java/com/jdbayer/prueba/PruebaNisumApplication.java
@@ -0,0 +1,13 @@
+package com.jdbayer.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaNisumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaNisumApplication.class, args);
+    }
+
+}
