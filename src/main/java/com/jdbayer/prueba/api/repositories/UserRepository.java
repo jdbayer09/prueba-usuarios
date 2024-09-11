126eb579ca7553258a3926c5cad913130ba4451c
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 17:26:47 -0500

    Configuracion de la entidad de usuario

---
+++ src/main/java/com/jdbayer/prueba/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jdbayer.prueba.api.repositories;
+
+import com.jdbayer.prueba.api.models.entities.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+}
