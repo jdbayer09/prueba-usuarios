7581545e5bfa7c2f8831b403e2f87ff6dcfd0374
Julian Bayer <jdbayer09@hotmail.com>
2024-09-11 20:50:27 -0500

    Confioguracion del Servicio para registrar usuario

--- src/main/java/com/jdbayer/prueba/api/services/UserService.java
+++ src/main/java/com/jdbayer/prueba/api/services/UserService.java
@@ -2,13 +2,14 @@ package com.jdbayer.prueba.api.services;
 
 import com.jdbayer.prueba.api.models.dto.UserDTO;
 import com.jdbayer.prueba.api.models.dto.UserDetailDTO;
+import com.jdbayer.prueba.api.models.requests.UserRequest;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
 
-    UserDTO createUser(UserDTO user);
+    UserDTO createUser(UserRequest user);
     UserDTO updateUser(UserDTO user);
     void validateSessionUser(UUID userId, String sessionToken);
     UserDetailDTO findUserByEmail(String email);
