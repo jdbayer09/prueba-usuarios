134200e705f25229e1d41dde49489be1858e8841
Julian Bayer <jdbayer09@hotmail.com>
2024-09-12 15:03:17 -0500

    Prueba de servicio de phones

---
+++ src/main/java/com/jdbayer/prueba/api/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.jdbayer.prueba.api.controllers;
+
+import com.jdbayer.prueba.api.models.requests.UserRequest;
+import com.jdbayer.prueba.api.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@Tag(name = "User", description = "User API")
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "401", description = "Credentials are required to access this resource", content =
+        @Content(
+                mediaType = MediaType.APPLICATION_JSON_VALUE,
+                schema = @Schema(implementation = ErrorResponse.class)))
+})
+@SecurityRequirement(name = "Bearer Authentication")
+@Validated
+@RequiredArgsConstructor
+public class UserController {
+
+        private final UserService userService;
+
+        @PostMapping("/register")
+        @ResponseStatus(HttpStatus.CREATED)
+        @Operation(summary = "Register User")
+        public ResponseEntity<?> registerUser(@RequestBody @Valid UserRequest user) {
+                return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
+        }
+}
